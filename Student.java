66d9b661f9e51a1cc219a2e9efadc582fa904d89
karunya <karunyasathiyamoorthy2007@gmail.com>
2026-06-20 15:42:17 +0530

    Day1 : added string

---
+++ Student.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Student
+{
+    String sname;
+    int mark1,mark2,mark3,mark4,mark5;
+    int total;
+    double average;
+    Student(String name,int mark1,int mark2,int mark3,int mark4,int mark5)
+    {
+       name=sname;
+       mark1=m1;
+       mark2=m2;
+       mark3=m3;
+       mark4=m4;
+       mark5=m5; 
+    }
+}
