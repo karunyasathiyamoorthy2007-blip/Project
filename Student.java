a05e6be14086836620519b63b6bb1f4e78546dad
karunya <karunyasathiyamoorthy2007@gmail.com>
2026-06-22 12:58:42 +0530

    Day 2

--- Student.java
+++ Student.java
@@ -1,19 +1,52 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 public class Student
 {
-    String sname;
-    int mark1,mark2,mark3,mark4,mark5;
-    int total;
-    double average;
-    Student(String name,int mark1,int mark2,int mark3,int mark4,int mark5)
+    public static void getData(int rollNo[],String name[],int total[],double average[],int n)
     {
-       name=sname;
-       mark1=m1;
-       mark2=m2;
-       mark3=m3;
-       mark4=m4;
-       mark5=m5; 
-       mark6=m6;
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("\nEnter Student"+(i+1)+"Deatails");
+            System.out.print("Roll No:");
+            rollNo[i]=sc.nextInt();
+            sc.nextLine();
+            System.out.print("Name:");
+            name[i]=sc.nextLine();
+            int m1,m2,m3,m4,m5;
+            System.out.print("Mark 1:");
+            m1=sc.nextInt();
+            System.out.print("Mark 2:");
+            m2=sc.nextInt();
+            System.out.print("Mark 3:");
+            m3=sc.nextInt();
+            System.out.print("Mark 4:");
+            m4=sc.nextInt();
+            System.out.print("Mark 5:");
+            m5=sc.nextInt();
+            total[i]=m1+m2+m3+m4+m5;
+            average[i]=total[i]/5.0;
+        }
     }
-}
+        static void sortData(int rollNo[],String name[],int total[],double average[],int n[]){
+            for(int i=0;i<n-1;i++)
+            {
+                for(int j=i+1;j<n;j++)
+                {
+                    if(total(i)<total(j))
+                    {
+                        int t=rollNo[i];
+                        rollNo[i]=rollNo[j];
+                        rollNo[j]=t;
+                        String s=name[i];
+                        name[i]=name[j];
+                        name[j]=s;
+                        t=total[i];
+                        total[i]=total[j];
+                        total[j]=t;
+                        double d=average[i];
+                        average[i]=average[j];
+                        average[j]=d;
+                    }
+                }
+            }
+        }
